6098f39a53956f232fc06f36d644687b57496c85
Ajay Kumar Yadav <yadavajay109458@gmail.com>
2022-05-18 14:14:41 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,11 +2,16 @@
 
 # TODO: Manually identify the clusters mapping for the partner's namespaces
 
-clusterRGNames=()
+declare -A subIdClusterRGNameMap=()
 configDaemonSetName="pid-configure-ds"
 
-for clusterRGName in clusterRGNames
-do
+for subscriptionId in "${!subIdClusterRGNameMap[@]}"
+do 
+   
+    clusterRGName = ${subIdClusterRGNameMap[$subscriptionId]}
+
+    az account set --subscription $subscriptionId
+
     # Set the cluster config
     az aks get-credentials --name ${clusterRGName}-aks --resource-group ${clusterRGName} --admin
 
