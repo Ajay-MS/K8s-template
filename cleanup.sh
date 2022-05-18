682c3b0f5eac61a0a1acdbeb61d24a024d1c2f2d
Ajay Kumar Yadav <yadavajay109458@gmail.com>
2022-05-18 14:29:17 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-# TODO: Manually identify the clusters mapping for the partner's namespaces
-
+# TODO: Extract clusters mapped to the partner namespace from Griffin D2 - Cluster Deployment - Inventory
 declare -A subIdClusterRGNameMap=()
+
+# Name of the daemonset need to be cleaned up
 configDaemonSetName="pid-configure-ds"
 
 for subscriptionId in "${!subIdClusterRGNameMap[@]}"
 do 
-   
+    # Extract cluster name
     clusterRGName = ${subIdClusterRGNameMap[$subscriptionId]}
 
+    # Set subscription to the current cluster subscription
     az account set --subscription $subscriptionId
 
     # Set the cluster config
     az aks get-credentials --name ${clusterRGName}-aks --resource-group ${clusterRGName} --admin
 
+    # Trigger delete for the daemonset
     kubectl delete daemonset ${configDaemonSetName}
 done
